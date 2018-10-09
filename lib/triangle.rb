386f7b11e0efc9544299d49a47a9e7de4e450e26
larryvsdata <ermanbekaroglu@gmail.com>
2018-10-09 03:52:26 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ def initialize (x1, x2, x3)
 end
 
 def valid?
-  @x1 + @x2 > @x3 && @x2 + @x3 > @x1 && @x1 + @x3 > @x2 && @x1 >0 && @x2 > 0 && @x3 > 0
+  @x1 + @x2 > @x3 && @x2 + @x3 > @x1 && @x1 + @x3 > @x2 && @x1 >0 
 end
 
 def kind 
