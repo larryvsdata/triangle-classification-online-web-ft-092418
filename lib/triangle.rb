c76f41db4e4350e2691eec0b397fb180b31cf93d
larryvsdata <ermanbekaroglu@gmail.com>
2018-10-09 03:28:47 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,16 @@
 class Triangle
 attr_accessor :x1, :x2 , :x3 , :equilateral , :isosceles , :scalene
 
+def initialize (x1, x2, x3)
+  @x1 = x1
+  @x2 = x2
+  @x3 = x3
+  
+end
+
+def valid?
+  @x1 + @x2 > @x3 && @x1 + @x2 > @x3
+end
 
 
 end
