d775ce08b7a8afd3649dd40e4e6166da717838cb
larryvsdata <ermanbekaroglu@gmail.com>
2018-10-09 03:42:55 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,10 +15,22 @@ end
 def kind 
   
   if self.valid?
-    
+    if @x1==@x2 && @x1==@x3
+      :equilateral
+    elsif @x1 == @x2 || @x2 == @x3 || @x1 == @x3
+      :isosceles
+    else
+      :scalene
+      
+    end
     
   else
     
+    begin
+        raise TriangleError
+      rescue TriangleError => error
+          puts error.message
+      end
     
   end
   
